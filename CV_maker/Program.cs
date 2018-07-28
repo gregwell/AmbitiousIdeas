d9a4387b78639cbf99521b4ee6804e74b780e7dc
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2018-07-28 22:34:54 +0200

    chunk and phrase working fine.

--- CV_maker/Program.cs
+++ CV_maker/Program.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.IO;
 
 namespace CV_maker
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            var doc = new Document(PageSize.A4);
+            PdfWriter.GetInstance(doc, new FileStream(@"D:\cv.pdf", FileMode.Create));
+
+            Chunk chunk = new Chunk("A chunk represents an isolated string.\n ");
+
+            doc.Open();
+            Phrase phrase = new Phrase();
+
+            for (int i = 0; i < 20; i++)
+            {
+                phrase.Add(chunk);
+                doc.Add(phrase);
+            }
+
+            doc.Close();
         }
     }
 }
